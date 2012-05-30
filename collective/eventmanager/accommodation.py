52e06b0a51ac755249f53336c8c468d7b7907c0c
Joel Kleier <joel.kleier@gmail.com>
2012-05-30 08:47:52 -0500

    accommodations now display a map when viewed

--- collective/eventmanager/accommodation.py
+++ collective/eventmanager/accommodation.py
@@ -1,9 +1,10 @@
+import string
+
 from five import grok
 from zope import schema
 from plone.directives import form
 from Products.CMFCore.utils import getToolByName
 from collective.geo.mapwidget.browser.widget import MapWidget
-from collective.geo.mapwidget.interfaces import IMapWidget
 from collective.z3cform.mapwidget.widget import MapFieldWidget
 
 from collective.eventmanager.interfaces import ILayer
@@ -21,8 +22,6 @@ class IAccommodation(form.Schema):
             required=False,
         )
 
-#class TravelAccommodationMapWidget()
-
 
 class View(grok.View):
     """Default view (called "@@view"") for an accommodation.
@@ -35,12 +34,35 @@ class View(grok.View):
     grok.name('view')
     grok.layer(ILayer)
 
+    MAP_CSS_CLASS = 'accommodationlocation'
+
     def __call__(self):
+        # setup map widget
         portal = getToolByName(self.context, "portal_url")
         mw = MapWidget(self, self.request, portal)
-        mw.mapid = 'location'
+        mw.mapid = 'accommodationlocation'
+        mw.addClass(self.MAP_CSS_CLASS)
         self.mapfields = [mw]
 
-        import pdb; pdb.set_trace()
-
         return super(View, self).__call__()
+
+    def cgmapSettings(self):
+        settings = {}
+
+        coords = [0, 0]
+        if self.context.location != None \
+                and self.context.location[0:6] == u'POINT(':
+            coords = string.split(self.context.location[6:-1], ' ')
+
+        settings['lon'] = float(coords[0])
+        settings['lat'] = float(coords[1])
+        settings['zoom'] = 16
+
+        return "cgmap.state['" + self.MAP_CSS_CLASS + "'] = " \
+               + str(settings) + ";"
+
+    def mapwidth(self):
+        return 'inherit'
+
+    def mapheight(self):
+        return '500px'
