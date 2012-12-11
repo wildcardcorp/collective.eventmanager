3c62547bd5fb23339caa48a291ca543955d55e8f
Joel Kleier <joel.kleier@gmail.com>
2012-12-11 12:42:53 -0600

    changed the map widget to be shown by default and hidden with a button click instead of hiding after a 1 second delay.

--- collective/eventmanager/browser/static/em.js
+++ collective/eventmanager/browser/static/em.js
@@ -1,28 +1,23 @@
 (function($){
 $(document).ready(function(){
+    var map = $('#map');
 
-    setTimeout(function(){
-        // need to wait for maps to get setup...
-        var map = $('#map');
+    if(map.length > 0){
+        var btn = $('<a id="map-btn" href="#" class="open">Hide Map</a>');
+        map.before(btn);
+        btn.click(function(){
+            if(!btn.hasClass('open')){
+                btn.addClass('open');
+                btn.html("Hide Map");
+                map.slideDown();
+            }else{
+                map.slideUp();
+                btn.html("Show Map");
+                btn.removeClass('open');
+            }
+            return false;
+        });
 
-        if(map.length > 0){
-            map.hide();
-            var btn = $('<a id="map-btn" href="#">Show Map</a>');
-            map.before(btn);
-            btn.click(function(){
-                if(!btn.hasClass('open')){
-                    btn.addClass('open');
-                    btn.html("Hide Map");
-                    map.slideDown();
-                }else{
-                    map.slideUp();
-                    btn.html("Show Map");
-                    btn.removeClass('open');
-                }
-                return false;
-            });
-
-        }
-    }, 1000);
+    }
 });
 })(jQuery);
